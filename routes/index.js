810cd3f36f4b0ec815fda8c8fe07575910028ec0
Zack Pitts <folkstring@hotmail.com>
2015-05-24 15:31:33 -0700

    Adding sms reply on rejection of incoming call

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var ACCOUNT_SID = '';
+var AUTH_TOKEN = '';
+var twilio = require('twilio');
+var client = twilio(ACCOUNT_SID, AUTH_TOKEN);
+
 router.get('/', function(req, res, next) {
 
-	var ACCOUNT_SID = '';
-	var AUTH_TOKEN = '';
-	var twilio = require('twilio');
 	var capability = new twilio.Capability(ACCOUNT_SID, AUTH_TOKEN);
 
 	capability.allowClientIncoming('zack');
+	capability.allowClientOutgoing('');
 
 	var token = capability.generate();
 
   res.render('index', { title: 'Twilio App', token: token });
+});
+
+router.post('/sms', function(req, res, next) {	
+	console.log(req.body);
+	client.sendMessage({
+
+	    to: req.body.to, 
+	    from: '', 
+	    body: req.body.body 
+
+	}, function(err, responseData) { 
+
+	    if (!err) { 
+	        console.log(responseData.from); 
+	        console.log(responseData.body); 
+	    } else {
+	    	console.log(JSON.stringify(err));
+	    }
+	});
+
+    res.end();
 });
 
 module.exports = router;
