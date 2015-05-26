3cf607afeabcafa16e8692c5cc4b3360cb5d4ba9
Zack Pitts <folkstring@hotmail.com>
2015-05-26 00:56:23 -0700

    Moving config data to env variables

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var ACCOUNT_SID = '';
-var AUTH_TOKEN = '';
+var ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+var AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+var TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+var TWILIO_APP = process.env.TWILIO_APP;
+
 var twilio = require('twilio');
 var client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 
@@ -11,7 +14,7 @@ router.get('/', function(req, res, next) {
 	var capability = new twilio.Capability(ACCOUNT_SID, AUTH_TOKEN);
 
 	capability.allowClientIncoming('zack');
-	capability.allowClientOutgoing('');
+	capability.allowClientOutgoing(TWILIO_APP);
 
 	var token = capability.generate();
 
@@ -23,7 +26,7 @@ router.post('/sms', function(req, res, next) {
 	client.sendMessage({
 
 	    to: req.body.to, 
-	    from: '', 
+	    from: TWILIO_NUMBER, 
 	    body: req.body.body 
 
 	}, function(err, responseData) { 
@@ -38,5 +41,31 @@ router.post('/sms', function(req, res, next) {
 
     res.end();
 });
+
+router.post('/message-test', function(req, res, next) {	
+
+	var message = {
+	    to: req.body.to, 
+	    from: TWILIO_NUMBER, 
+	    body: req.body.body,
+	}
+
+	if (req.body.mediaUrl.length){
+		message.mediaUrl = req.body.mediaUrl;
+	}
+
+	client.messages.post(message, function(err, responseData) { 
+
+	    if (!err) { 
+	        console.log(responseData.from); 
+	        console.log(responseData.body); 
+	    } else {
+	    	console.log(JSON.stringify(err));
+	    }
+	});
+
+    res.end();
+});
+
 
 module.exports = router;
